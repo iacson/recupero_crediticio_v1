817536d7ec953ce6efc1020b336bfc0ac4364305
Unknown <lucas.iacson@hotmail.com>
2018-12-09 04:15:04 -0300

    CRUD Model

---
+++ application/models/Crud_model.php
@@ -0,0 +1,71 @@
+<?php
+
+class Crud_model extends CI_Model {
+	
+    function __construct() {
+        parent::__construct();
+    }
+	
+	function getData($table)
+	{
+		$query = $this->db->get($table)->result_array();
+		return $query;
+	}
+
+	function getDataResultById($table, $field, $id)
+	{
+		$this->db->where($field, $id);
+		$query = $this->db->get($table)->result_array();
+		return $query;
+	}
+
+	function getDataById($table, $field, $id)
+	{
+		$this->db->where($field, $id);
+		$query = $this->db->get($table)->row_array();
+		return $query;
+	}
+
+	function insertData($table, $data)
+	{
+		if(	$this->db->insert($table, $data) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	function insertDataIdResponse($table, $data)
+	{
+		$this->db->insert($table, $data);
+		return $this->db->insert_id();
+	}
+
+	function updateData($table, $field, $id, $data)
+	{
+		$this->db->where($field, $id);
+		if(	$this->db->update($table, $data) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function deleteData($table, $field, $id)
+	{
+		$this->db->where($field, $id);
+		if(	$this->db->delete($table) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	function countData($table, $field, $value)
+	{
+		$this->db->where($field, $value);
+		$this->db->from($table);
+		return $this->db->count_all_results();
+	}
+
+}
